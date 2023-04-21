000b14ef0c30201f992e5ad3e6ca9e445ac95c9d
Nahuel-circulo <pedroso.nahuel.dev@gmail.com>
2023-04-21 11:34:01 -0300

    refactoring code

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,59 @@
 import exerciseIcon from './assets/icon-exercise.svg';
 import playIcon from './assets/icon-play.svg';
-import selfCareIcon from './assets/icon-self-care.svg';
+import selfcareIcon from './assets/icon-self-care.svg';
 import socialIcon from './assets/icon-social.svg';
 import studyIcon from './assets/icon-study.svg';
 import workIcon from './assets/icon-work.svg';
 
+import data from './data/data.js';
+interface dashboardData {
+  title: string,
+  timesframe: {
+    dayly: {
+      current: number
+      previus: number
+    },
+    weekly: {
+      current: number
+      previus: number
+    },
+    monthly: {
+      current: number
+      previus: number
+    },
+  }
+}
+
 function App() {
+
+  const iconReturn = (word: string) => {
+
+    switch (word) {
+      case 'Work':
+        return workIcon
+
+      case 'Study':
+        return studyIcon
+
+      case 'Play':
+        return playIcon
+
+      case 'Exercise':
+        return exerciseIcon
+
+      case 'Social':
+        return socialIcon
+
+      case 'Self Care':
+        return selfcareIcon
+
+
+      default:
+        return workIcon
+
+    }
+  }
+
 
   return (
     <main>
@@ -16,57 +64,32 @@ function App() {
           <div className="card-body"></div>
         </div>
 
-        <div className="card" id="work-card">
-          <img src={workIcon} alt="work-icon" />
-          <div className="card-body">
+        {
+          data.map((item: dashboardData) => (
 
-            
-          </div>
-        </div>
 
-        <div className="card" id="play-card">
-          <img src={playIcon} alt="play-icon" />
-          <div className="card-body">
 
-            
-          </div>
-        </div>
+            <div className="card" id={`${item.title.replace(/\s+/g, '').toLowerCase()}-card`}>
+              <img src={iconReturn(item.title)} alt="work-icon" />
+              <div className="card-body">
+                <h3>{item.title}</h3> <button>...</button>
+                <h2>32</h2> <p>asd asd asd</p>
 
-        <div className="card" id="study-card">
-          <img src={studyIcon} alt="study-icon" />
-          <div className="card-body">
+              </div>
+            </div>
+          )
 
-            
-          </div>
-        </div>
+          )
+        }
 
-        <div className="card" id="exercise-card">
-          <img src={exerciseIcon} alt="exercise-icon" />
-          <div className="card-body">
 
-            
-          </div>
-        </div>
-
-        <div className="card" id="social-card">
-          <img src={socialIcon} alt="social-icon" />
-          <div className="card-body">
-
-            
-          </div>
-        </div>
-
-        <div className="card" id="self-care-card">
-          <img src={selfCareIcon} alt="self-care-icon" />
-          <div className="card-body">
-
-            
-          </div>
-        </div>
 
       </div>
     </main>
   )
 }
+
+
+
 
 export default App
