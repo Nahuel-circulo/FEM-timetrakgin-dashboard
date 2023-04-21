4694ee12de4af7c30df13aac97e1aff8c730293b
Nahuel-circulo <pedroso.nahuel.dev@gmail.com>
2023-04-21 13:05:43 -0300

    mobile design

--- src/App.tsx
+++ src/App.tsx
@@ -4,86 +4,90 @@ import selfcareIcon from './assets/icon-self-care.svg';
 import socialIcon from './assets/icon-social.svg';
 import studyIcon from './assets/icon-study.svg';
 import workIcon from './assets/icon-work.svg';
+import ellipsisIcon from './assets/icon-ellipsis.svg';
 
 import data from './data/data.js';
+import { useState } from 'react';
+
+interface Timeframe {
+  current: number
+  previous: number
+}
 interface dashboardData {
   title: string,
-  timesframe: {
-    dayly: {
-      current: number
-      previus: number
-    },
-    weekly: {
-      current: number
-      previus: number
-    },
-    monthly: {
-      current: number
-      previus: number
-    },
-  }
+  timeframes: {
+    daily: Timeframe,
+    weekly: Timeframe,
+    monthly: Timeframe
+  },
 }
 
+
 function App() {
+
+  const [period, setPeriod] = useState('Week');
+
+
+  /*   const periodReturn = (period: string) => {
+  
+      switch (period) {
+        case :
+          
+          break;
+      
+        default:
+          break;
+      }
+    } */
 
   const iconReturn = (word: string) => {
 
     switch (word) {
       case 'Work':
         return workIcon
-
       case 'Study':
         return studyIcon
-
       case 'Play':
         return playIcon
-
       case 'Exercise':
         return exerciseIcon
-
       case 'Social':
         return socialIcon
-
       case 'Self Care':
         return selfcareIcon
-
-
       default:
         return workIcon
-
     }
   }
-
 
   return (
     <main>
       <div className="dashboard">
-
-        <div className="card" id="profile-card">
-          <h1>Profile</h1>
-          <div className="card-body"></div>
+        <div className="profile-card">
+          <div className="profile-card__body">
+            <img src="/public/image-jeremy.png" alt="profile picture" />
+            <p>Report for</p>
+            <h3 className='profile-card__username'>Jeremy Robson</h3>
+          </div>
+          <div className='profile-card__actions'>
+            <button className={`${period === 'Day' ? 'active' : ''} btn`}>Daily</button>
+            <button className={`${period === 'Week' ? 'active' : ''} btn`}>Weekly</button>
+            <button className={`${period === 'Month' ? 'active' : ''} btn`}>Monthly</button>
+          </div>
         </div>
-
         {
           data.map((item: dashboardData) => (
-
-
-
-            <div className="card" id={`${item.title.replace(/\s+/g, '').toLowerCase()}-card`}>
-              <img src={iconReturn(item.title)} alt="work-icon" />
+            <div key={item.title} className="card" id={`${item.title.replace(/\s+/g, '').toLowerCase()}-card`}>
+              <img className='card-image' src={iconReturn(item.title)} alt="work-icon" />
               <div className="card-body">
-                <h3>{item.title}</h3> <button>...</button>
-                <h2>32</h2> <p>asd asd asd</p>
-
+                <h3 className='card-body__title'>{item.title}</h3>
+                <img src={ellipsisIcon} alt="dots" />
+                <h2 className='card-body__currently'>{item.timeframes.daily.current}hrs</h2>
+                <p className='card-body__last'>Last {period}-{item.timeframes.daily.previous}hrs</p>
               </div>
             </div>
-          )
-
-          )
+          ))
         }
-
-
-
       </div>
     </main>
   )
